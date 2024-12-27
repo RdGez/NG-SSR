a037f64a7951dbabdde062309cdaa6524d9364aa
Oscar Rodriguez <rdgez.oscar@gmail.com>
2024-12-27 14:11:08 -0600

    feature: Add unit testing for app component and routes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,53 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+@Component({
+    selector: 'navbar',
+    standalone: true,
+})
+class NavbarComponentMock {}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compile: HTMLDivElement;
+  let app: AppComponent;
+
   beforeEach(async () => {
+    // One way to work with Mocks:
+    // TestBed.overrideComponent(AppComponent, {
+    //   set: {
+    //     imports: [NavbarComponentMock],
+    //     schemas: [ CUSTOM_ELEMENTS_SCHEMA]
+    //   }
+    // })
+
+    // Recomented way to work with Mocks:
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter([])]
+    }).overrideComponent(AppComponent, {
+      add: {
+        imports: [NavbarComponentMock]
+      },
+      remove: {
+        imports: [NavbarComponent]
+      }
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    compile = fixture.nativeElement;
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ssr_app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ssr_app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ssr_app');
+  it(`should render the navbar and router-outlet`, () => {
+    expect(compile.querySelector('navbar')).toBeTruthy();
+    expect(compile.querySelector('router-outlet')).toBeTruthy();
   });
 });
